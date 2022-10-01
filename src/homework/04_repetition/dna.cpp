0d4699986ffcb9ed0b4d7ab8505367d6916ff81e
EricThompson2 <eric.thompson2@g.austincc.edu>
2022-10-01 15:27:10 +0000

    did homework 4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,26 @@
 //add include statements
 
 //add function(s) code here
+int factorial(int num) {
+    int sum = 1;
+    while (num > 0) {
+      sum *= num;
+      num--;
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2) {
+  int numSwap;
+  while (num1 != num2) {
+    if (num1 < num2){
+      numSwap = num1;
+      num1 = num2;
+      num2 = numSwap;
+    }
+    else if (num1 > num2) {
+      num1 -= num2;
+    }
+  }
+  return num1;
+}
