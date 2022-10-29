bc6a5f55f5686e0b0fcb5d3692fc708cb6e89ea2
EricThompson2 <eric.thompson2@g.austincc.edu>
2022-10-29 03:36:06 +0000

    Homework 6

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,24 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify tic_tac_toe class's") {
+	tic_tac_toe game;
+	int counter = 1;
+
+	game.start_game("X");
+
+	while (counter < 10) {
+		//game.display_board();
+
+		REQUIRE(game.game_over() == false);
+		game.mark_board(counter);
+		counter++;
+	}
+	//game.display_board();
+	REQUIRE(game.game_over() == true);
+}
